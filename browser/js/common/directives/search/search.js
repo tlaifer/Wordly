658278cc4b2ba44b77863f482f95470ac24114a8
john-mcgowan1992 <mcgowanj92@gmail.com>
2016-03-16 15:27:23 -0700

    rendering word data from dict and thesaurus

--- browser/js/common/directives/search/search.js
+++ browser/js/common/directives/search/search.js
@@ -3,8 +3,19 @@ app.directive('searchbar', (addWordFactory) => {
 		restrict: 'E',
 		templateUrl: 'js/common/directives/search/search.html',
 		link: (scope) => {
-			scope.searchDict = addWordFactory.searchDictionary;
-			scope.searchThesauraus = addWordFactory.searchThesaurus;
+			scope.wordData;
+			scope.searchDict = (word) => {
+				return addWordFactory.searchDictionary(word)
+						.then(data => {
+							scope.wordData = data;
+						})
+			};
+			scope.searchThesaurus = (word) => {
+				return addWordFactory.searchThesaurus(word)
+						.then(data => {
+							scope.wordData = data;
+						})
+			};
 			scope.searchInput = '';
 		}
 	}
