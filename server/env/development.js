259b390268de3862dc09228b3577341d337a2dc7
john-mcgowan1992 <mcgowanj92@gmail.com>
2016-04-02 00:03:01 -0700

    started oauth

--- server/env/development.js
+++ server/env/development.js
@@ -12,8 +12,8 @@ module.exports = {
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    "clientID": "94269562487-gm0f3otf7mjmjaat8e70llpfp6pjpeg0.apps.googleusercontent.com",
+    "clientSecret": "w7aerqvsDZIDtKu7KWh7234m",
+    "callbackURL": "http://localhost:1337/auth/google/callback"
   }
 };
