1095dfa3cdf428b1998c8b696ac0e1f8e9822ae3
john-mcgowan1992 <mcgowanj92@gmail.com>
2016-04-21 13:58:12 -0700

    removed env variables

--- server/env/development.js
+++ server/env/development.js
@@ -1,16 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+  "DATABASE_URI": "mongodb://localhost:27017/wordly",
   "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
-  },
-  "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
-  },
   "GOOGLE": {
     "clientID": "94269562487-gm0f3otf7mjmjaat8e70llpfp6pjpeg0.apps.googleusercontent.com",
     "clientSecret": "w7aerqvsDZIDtKu7KWh7234m",
